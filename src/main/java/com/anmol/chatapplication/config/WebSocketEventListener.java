e3eb1d190c7d524153b5c6e0b5ad9a1872bd84a4
anmol11909 <singh1190942175@gmail.com>
2025-03-17 16:52:08 +0530

    implemented First page (frontend)

--- src/main/java/com/anmol/chatapplication/config/WebSocketEventListener.java
+++ src/main/java/com/anmol/chatapplication/config/WebSocketEventListener.java
@@ -4,6 +4,8 @@ import com.anmol.chatapplication.chat.ChatMessage;
 import com.anmol.chatapplication.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -12,12 +14,18 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
+//@Slf4j
 
 // to inform users that a user has left the chat
 public class WebSocketEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
 
     private final SimpMessageSendingOperations messagingTemplate;
+
+    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
 
     @EventListener
@@ -28,7 +36,7 @@ public class WebSocketEventListener {
         String username = headerAccessor.getSessionAttributes().get("username").toString(); // because its an object so .toString
 
         if(username!=null){
-            log.info("User disconnected:{}", username);
+            logger.info("User disconnected:{}", username);
             var chatMessage  = ChatMessage.builder()
                     .type(MessageType.LEAVE)
                     .sender(username)
